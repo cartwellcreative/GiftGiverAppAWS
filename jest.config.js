f5253a31ea2973b94cd93b7a73fd5485255a0da8
Sarah Carter <cartwellcreative@gmail.com>
2024-09-05 00:01:51 -0400

    functioning helloworld api in aws

---
+++ jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    preset: 'ts-jest',
+    testEnvironment: 'node',
+    moduleFileExtensions: ['ts', 'js'],
+    testMatch: ['**/?(*.)+(spec|test).[tj]s?(x)'],
+    transform: {
+      '^.+\\.ts$': 'ts-jest'
+    }, 
+    testPathIgnorePatterns: [
+      "/node_modules/",
+      "/dist/"
+    ],
+  };
